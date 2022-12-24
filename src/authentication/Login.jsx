9763fd3594ce94428bbfe5bc669f857f49026dab
Metin KONUK <metinkonuk@hotmail.com>
2022-12-24 21:00:00 +0300

    design & implement the logic of Login component

--- src/authentication/Login.jsx
+++ src/authentication/Login.jsx
@@ -1,132 +1,205 @@
-import * as React from 'react';
+/* eslint-disable prefer-regex-literals */
+/* eslint-disable no-unused-vars */
+import React, { useState, useEffect } from 'react';
 import {
   Avatar,
   Button,
-  CssBaseline,
   TextField,
   FormControlLabel,
   Checkbox,
-  Link,
-  Paper,
   Box,
   Grid,
   Typography,
+  Radio,
+  RadioGroup,
 } from '@mui/material';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { useSnackbar } from 'notistack';
 import { indigo } from '@mui/material/colors';
+import {
+  signInWithGoogle,
+  loginWithEmailAndPassword,
+  signInWithFacebook,
+  signInWithGithub,
+} from '../firebase';
+import googleIcon from './icons/google.png';
+import facebookIcon from './icons/facebook.png';
+import githubIcon from './icons/github.png';
+import headerIcon from './icons/header.png';
 
-function Copyright(props) {
-  return (
-    // eslint-disable-next-line react/jsx-props-no-spreading
-    <Typography variant="body2" color="text.main" align="center" {...props}>
-      {'Copyright ©  '}
-      <Link color="inherit" href="https://google.com">
-        NHP İLAÇ
-      </Link>
-      {`  ${new Date().getFullYear()}`}
-    </Typography>
-  );
-}
-
-const theme = createTheme();
-const image = 'https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80';
-
-// send and retrieve credentials from database here
 function Login() {
+  const { enqueueSnackbar } = useSnackbar();
+  const [position, setPosition] = useState(null);
+  const [credentials, setCredentials] = useState({});
+
+  const checkDataValidity = (data) => {
+    const { email, password } = data;
+    const emailAddressValidator = new RegExp('^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$');
+    const passwordValidator = new RegExp('^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$');
+    if (!emailAddressValidator.test(email)) {
+      enqueueSnackbar('Email address format is not valid!', { variant: 'warning' });
+      return false;
+    }
+    if (!passwordValidator.test(password)) {
+      enqueueSnackbar('Password must be at least 8 characters, contain at least one number, one uppercase and one lowercase letter!', { variant: 'warning' });
+      return false;
+    }
+    return true;
+  };
   const handleSubmit = (event) => {
     event.preventDefault();
-    // const data = new FormData(event.currentTarget);
-    // console.log({
-    //   identity: data.get('identity'),
-    //   password: data.get('password'),
-    // });
+    const data = {
+      email: credentials.email,
+      password: credentials.password,
+    };
+    if (checkDataValidity(data)) {
+      if (position === 'employee') {
+        //
+      } else if (position === 'manager') {
+        //
+      } else {
+        //
+      }
+    }
   };
-
+  const handleChange = (event) => {
+    setCredentials({ ...credentials, [event.target.name]: event.target.value });
+  };
+  const handlePositionChange = (event) => {
+    setPosition(event.target.value);
+  };
   return (
-    <ThemeProvider theme={theme}>
-      <Grid container component="main" sx={{ height: '100vh' }}>
-        <CssBaseline />
-        <Grid
-          item
-          xs={false}
-          sm={4}
-          md={7}
-          sx={{
-            backgroundImage: `url(${image})`,
-            backgroundRepeat: 'no-repeat',
-            backgroundColor: (t) => (t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900]),
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
-          }}
+    <Box
+      sx={{
+        my: 8,
+        mx: 4,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+      }}
+    >
+      <Avatar sx={{ width: 150, height: 150 }} src={headerIcon} />
+      <Typography component="h1" variant="h5">
+        Sign In
+      </Typography>
+      <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
+        <TextField
+          name="email"
+          id="email"
+          label="Email"
+          type="email"
+          required
+          fullWidth
+          size="medium"
+          margin="normal"
+          autoFocus
+          onChange={handleChange}
         />
-        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-          <Box
+        <TextField
+          name="password"
+          id="password"
+          label="Password"
+          type="password"
+          required
+          fullWidth
+          size="medium"
+          margin="normal"
+          onChange={handleChange}
+        />
+        <Typography component="h1" variant="h5" align="center" sx={{ mt: 1 }}>
+          Select position you work on
+        </Typography>
+        <Grid
+          sx={{ mt: 1 }}
+          container
+          spacing={0}
+          direction="row"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <RadioGroup
+            defaultValue="employee"
+            onChange={handlePositionChange}
+          >
+            <FormControlLabel
+              name="developer"
+              control={<Radio value="developer" />}
+              label="Developer"
+            />
+            <FormControlLabel
+              control={<Radio value="manager" />}
+              name="manager"
+              label="Manager"
+            />
+            <FormControlLabel
+              name="employee"
+              control={<Radio value="employee" />}
+              label="Employee"
+            />
+          </RadioGroup>
+          <Button
+            type="submit"
+            variant="contained"
+            fullWidth
             sx={{
-              my: 8,
-              mx: 4,
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
+              mt: 3.5, mb: 0.5, bgcolor: indigo[700], borderRadius: 0, boxShadow: 0, '&:hover': { backgroundColor: indigo[500] },
             }}
           >
-            <Avatar sx={{ m: 1, bgcolor: indigo[900] }}>
-              <LockOutlinedIcon />
-            </Avatar>
-            <Typography component="h1" variant="h5">
-              Sign in
-            </Typography>
-            <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                id="identity"
-                label="Identity Number"
-                name="identity"
-                autoComplete="identity"
-                autoFocus
-              />
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                name="password"
-                label="Password"
-                type="password"
-                id="password"
-                autoComplete="current-password"
-              />
-              <FormControlLabel
-                control={<Checkbox value="remember" color="primary" />}
-                label="Remember me"
-              />
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2, bgcolor: indigo[900] }}
-              >
-                Sign In
-              </Button>
-              <Grid container>
-                <Grid item xs>
-                  <Link href="https://google.com" variant="body2">
-                    Forgot password?
-                  </Link>
-                </Grid>
-                <Grid item>
-                  <Link href="https://google.com" variant="body2">
-                    Don&apos;t have an account? Require!
-                  </Link>
-                </Grid>
-              </Grid>
-              <Copyright sx={{ mt: 5 }} />
-            </Box>
-          </Box>
+            Sign In
+          </Button>
         </Grid>
-      </Grid>
-    </ThemeProvider>
+        <Grid
+          sx={{ mt: 1 }}
+          container
+          spacing={2}
+          direction="row"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <Grid item xs={4}>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{
+                mt: 0.5, mb: 0.5, bgcolor: 'white', borderRadius: 0, boxShadow: 0, '&:hover': { backgroundColor: 'white' },
+              }}
+              onClick={signInWithGithub}
+              startIcon={<Avatar src={githubIcon} />}
+            >
+              {null}
+            </Button>
+          </Grid>
+          <Grid item xs={4}>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{
+                mt: 0.5, mb: 0.5, bgcolor: 'white', borderRadius: 0, boxShadow: 0, '&:hover': { backgroundColor: 'white' },
+              }}
+              onClick={signInWithFacebook}
+              startIcon={<Avatar src={facebookIcon} />}
+            >
+              {null}
+            </Button>
+          </Grid>
+          <Grid item xs={4}>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{
+                mt: 0.5, mb: 0.5, bgcolor: 'white', borderRadius: 0, boxShadow: 0, '&:hover': { backgroundColor: 'white' },
+              }}
+              onClick={signInWithGoogle}
+              startIcon={<Avatar src={googleIcon} />}
+            >
+              {null}
+            </Button>
+          </Grid>
+        </Grid>
+      </Box>
+    </Box>
   );
 }
 
